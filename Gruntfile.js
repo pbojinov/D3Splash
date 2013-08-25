f3e96009a21195a4012808ba5d7bc537e1d951a1
pbojinov <petarbojinov@gmail.com>
2013-08-24 21:08:14 -0700

    almost done with build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,16 +26,19 @@ module.exports = function (grunt) {
             all: ['app/js/recipe.js', 'app/js/recipe.clean.js'] //'Apps/LifeAlly/js/update-<%= pkg.version %>.min.js'
         },
         concat: {
-            options: {
-                separator: ';'
-            },
             js: {
                 src: ['app/js/jquery.js', 'app/js/bootstrap.min.js', 'app/js/magnific-popup.js', 'app/js/tools.js'],
-                dest: 'dist/js/d3splash-v<%= pkg.version %>.js'
+                dest: 'dist/js/d3splash-v<%= pkg.version %>.js',
+                options: {
+                    separator: ';'
+                }
             },
             css: {
-                src: ['app/css/bootstrap.min.css', 'app/css/navbar-fixed-top.css', 'app/css/magnific-popup.css', 'css/animate-custom.css'],
-                dest: 'dist/css/d3splash-v<%= pkg.version %>.css'
+                src: ['app/css/bootstrap.min.css', 'app/css/navbar-fixed-top.css', 'app/css/magnific-popup.css', 'app/css/animate-custom.css'],
+                dest: 'dist/css/d3splash-v<%= pkg.version %>.css',
+                options: {
+                    separator: ' '
+                }
             }
         },
         uglify: {
@@ -56,6 +59,32 @@ module.exports = function (grunt) {
                 src: 'dist/css/d3splash-v<%= pkg.version %>.css',
                 dest: 'dist/css/d3splash-v<%= pkg.version %>.min.css'
             }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'app/index.html'
+                }
+            }
+        },
+        copy: {
+            main: {
+                src: 'app/*.html',
+                dest: 'dist/',
+                flatten: true,
+                expand: true,
+                filter: 'isFile'
+            }
+        },
+        useminPrepare: {
+            html: ['dist/index.html']
+        },
+        usemin: {
+            html: ['dist/index.html']
         }
     });
 
@@ -65,7 +94,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-usemin');
 
     // Default task(s).
     grunt.registerTask('dev', ['jshint', 'concat', 'uglify', 'cssmin']);
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin', 'copy', 'usemin']);
 };
